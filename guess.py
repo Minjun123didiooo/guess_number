37a7332c172554737442918e9005461ac09709ae
Minjun <didioooc12@gmail.com>
2020-08-27 22:01:02 +0800

    new_version

--- guess.py
+++ guess.py
@@ -3,8 +3,12 @@
 #若猜錯 印出'比答案大/小'
 
 import random
+start = input('請決定隨機數字範圍開始值: ')
+start = int(start)
+end = input('請決定隨機數字範圍結束值: ')
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1
@@ -12,6 +16,7 @@ while True:
 	number = int(number)
 	if number == r:
 			print('你猜中了!')
+			print('這是你猜的第', count, '次')
 			break
 	elif number > r:
 		print('比答案大')
